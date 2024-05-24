cdd329bd478c32f2d4f5828f2fa7e1c61f619213
AbanoubEhab <36665223+AbanoubEhab@users.noreply.github.com>
2024-05-24 14:45:59 +0300

    Update qouta.py
    fix after te.eg website update

--- qouta.py
+++ qouta.py
@@ -8,91 +8,113 @@ from datetime import date
 import pywhatkit
 from pynput.keyboard import Key, Controller
 import mouse
+import random
 
 driver = webdriver.Firefox()
 
-driver.get("https://my.te.eg/user/login")
-
-nums = [["Line 1","Phone Number","Password"],
-        ["Line 2","Phone Number","Password"],
+nums = [["line name","number","password"],
+        ["line name","number","password"]
         ]
 
 report = ""
+keyboard = Controller()
+
+
+#logout function
+def logoutfun():
+    dropdown = WebDriverWait(driver, 10).until(
+       EC.presence_of_element_located((By.XPATH, "//*[@id='firstHeader']/div/div/div[2]/div[3]/div/div/div[2]/div/span"))
+    )
+    dropdown.click()
+
+    logout = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//div[@style='background-color: initial; cursor: pointer; border: 3px solid transparent; height: 2.5rem; display: flex; align-items: center; justify-content: center;']"))
+    )
+    logout.click()
+
+#send whatsapp function
+def sendwhatsapp(phone):
+    pywhatkit.sendwhatmsg_instantly(phone,report,tab_close=True,close_time=15)
+    sleep(15)
+    sleep(round(random.uniform(0.2, 3), 2))
+    mouse.click('left')
+    sleep(round(random.uniform(0.2, 1.5), 2))
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
 
 for num in nums:
 
     try:
+        #login
+        driver.get("https://my.te.eg/user/login")
         #Enter phone number
-        numbox = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "login-service-number-et"))
+        numbox = WebDriverWait(driver, 20).until(
+            EC.presence_of_element_located((By.ID, "login_loginid_input_01"))
         )
         numbox.clear()
         numbox.send_keys(num[1]) 
-        numbox.send_keys(Keys.TAB,Keys.ARROW_DOWN)
+        sleep(0.5)
+        numbox.send_keys(Keys.TAB,Keys.ARROW_DOWN,Keys.RETURN)
 
         #Enter password
         passbox = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "login-password-et"))
+            EC.presence_of_element_located((By.ID, "login_password_input_01"))
         )
         passbox.clear()
         passbox.send_keys(num[2],Keys.RETURN)
 
         try:
             #Check if qouta is empty
-            cancel = WebDriverWait(driver, 7.5).until(
-            EC.presence_of_element_located((By.XPATH, "//button[@class='bg-transparent border-0 text-color p-button p-component']"))
+            IsEmpty = WebDriverWait(driver, 7.5).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class='ant-modal-body']"))
             )
-            cancel.click()
-
-            sleep(2.5)
+            #pressing cancel
+            #cancel = WebDriverWait(driver, 7.5).until(
+            #EC.presence_of_element_located((By.XPATH, "//button[@class='ant-btn ant-btn-link ec_button_ant-btn-link_4Dr4uc']"))
+            #)
+            #cancel.click()
+            #reload page to remove the popup
+            driver.get("https://my.te.eg/user/login")
+            sleep(2)
             #Logout
-            dropdown = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//span[@class='p-submenu-icon pi pi-angle-down ng-star-inserted']"))
-            )
-            dropdown.click()
-
-            logout = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "logout-btn"))
-            )
-            logout.click()
-            report = report + num[0] +" "+ num[1] +" : Empty Qouta \n---------------------------------------"
+            logoutfun()
+            print("\n" + num[0] +" "+ num[1] +" : Empty Qouta \n---------------------------------------")
+            report = report + "\n" + num[0] +" "+ num[1] +" : Empty Qouta \n---------------------------------------"
 
 
         except:
             #Reading Qouta
             qouta = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "usage"))
+                EC.presence_of_element_located((By.XPATH, "//*[@id='_bes_window']/main/div/div/div[2]/div[3]/div/div/div[1]/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div[1]/span[1]"))
             )
 
             qoutaGB = qouta.text.split()[0]
-            print(str(num[0]) +" "+ str(num[1]) +"\n qouta = " + qoutaGB+ " GB ")
-            report = report + "\n" + str(num[0]) +" "+ str(num[1]) +"\n qouta = " + qoutaGB + " GB "
+            print(str(num[0]) +" "+ str(num[1]) +"\n Qouta = " + qoutaGB+ " GB ")
+            report = report + "\n" + str(num[0]) +" "+ str(num[1]) +"\n Qouta = " + qoutaGB + " GB "
 
             #Reading Renewal Date
-            driver.get("https://my.te.eg/offering/overview")
+            driver.get("https://my.te.eg/echannel/#/overview")
 
+            sleep(1)
             days = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "mr-auto"))
+                EC.presence_of_element_located((By.XPATH, "//*[@id='_bes_window']/main/div/div/div[3]/div[2]/div/div/div/div/div[4]/div/span"))
             )
-            print(days.text+"\n--------------------------------------------")
             report = report + days.text + "\n--------------------------------------------"
+            print(days.text + "\n--------------------------------------------")
 
             #Logout
             sleep(1)
-            dropdown = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//span[@class='p-submenu-icon pi pi-angle-down ng-star-inserted']"))
-            )
-            dropdown.click()
-
-            logout = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.ID, "logout-btn"))
-            )
-            logout.click()
+            logoutfun()
             
     except:
         #skipping if Error happens
         print("Error in " + num[0] +" "+ num[1] + "\n-----------------------------------------")
         report = report + "\nError in " + num[0] +" "+ num[1] + "\n-----------------------------------------"
+        try:
+            logoutfun()
+        except:
+            print("try recover")
 
 driver.quit()
 
@@ -103,10 +125,5 @@ file.write(report)
 file.close() 
 
 #send report on whatsapp
-keyboard = Controller()
-pywhatkit.sendwhatmsg_instantly("Mobile_Number",report,tab_close=True)
-sleep(2)
-mouse.click('left')
-sleep(1)
-keyboard.press(Key.enter)
-keyboard.release(Key.enter)
+sendwhatsapp("+2whatsapp number")
+
